d007b97c71626d45bdcacb519ea9f5ded4762b11
ahhmay <jeurkaramey@gmail.com>
2022-09-19 00:52:35 +0530

    (18/09)- Renamed Folder

--- src/components/PostCard/FeedHeader/FeedHeaderOptions/FeedHeaderOptions.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function FeedHeaderOptions() {
-  return (
-    <span className="d-flex justify-content-end p-2">&#8942;</span>
-  )
-}
-export default FeedHeaderOptions;
---
+++ src/services/delete_requests.service.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const deletePost = async (imgId) => {
+    // return await axios.delete(`${process.env.REACT_APP_HEROKU_URL}/post/delete_post/${imgId}`);
+    return await axios.delete(`http://localhost:8080/post/delete_post/${imgId}`);
+}
