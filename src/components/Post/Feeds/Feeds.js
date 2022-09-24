f29353469a0afd36dd683204b9f94ab56b1062cb
ahhmay <jeurkaramey@gmail.com>
2022-09-24 22:27:27 +0530

    (24/09) - Spinner added

--- src/components/Post/Feeds/Feeds.js
+++ src/components/Post/Feeds/Feeds.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Loader from '../../Loader/Loader';
 import AddFeed from '../../AddFeed/AddFeed';
 import FeedBody from '../FeedBody/FeedBody';
+import { useNavigate } from 'react-router-dom';
 import FeedFooter from '../FeedFooter/FeedFooter';
 import FeedHeader from '../FeedHeader/FeedHeader';
 import { getTimelineFeeds } from '../../../services/get_requests.service';
 import './Feeds.css';
-import Header from '../../Header/Header';
 
 function Feeds() {
-  const [response, setResponse] = useState([]);
+  const navigate = useNavigate();
+  const [response, setResponse] = useState(null);
   const [responseUpdated, setResponseUpdated] = useState(false);
 
   useEffect(() => {
@@ -17,24 +19,41 @@ function Feeds() {
   
   const getData = async () => {
     let response = await getTimelineFeeds();
-    setResponse(response?.data?.feeds);
+    setResponse(response?.data);
   }
   
-  if(response?.length === 0) {
-    return <div className="d-flex justify-content-center align-items-center fw-bold my-2 fs-3">Please wait. While feeds load :)</div>
+  if(!response?.statusCode) {
+    return (
+      <>
+        <div className="d-flex flex-column justify-content-center align-items-center fw-bold my-2 fs-12">
+          <Loader width={10} height={10} color={"grey"}/>
+          <span className="mt-1">Please wait. While data loads :)</span>
+        </div>
+      </>
+    )
+  }
+
+  const navigateToSearch = () => {
+    navigate('/search');
   }
 
   return (
     <div className="container">
-      {/* <div>
-        <Header />
-      </div> */}
       <div className="wrapper-container">
         <div className="feed-container">
           <AddFeed responseUpdated={responseUpdated} setResponseUpdated={setResponseUpdated}/>
         </div>
         {
-          response.map(feed => {
+          (response?.statusCode && response?.feeds.length===0) && (
+            <>
+              <div className="d-flex flex-column justify-content-center align-items-center fw-bold my-2 fs-12">
+                <span className="mt-1 text-align-center"><b className="hightlight" onClick={navigateToSearch}>Click here</b> to follow other users on social, or add new posts&#x261D;&#x261D;.</span>
+              </div>
+            </>
+          )
+        }
+        {
+          response?.feeds.map(feed => {
             return (
               <div className="feed-container" key={feed._id}>
                 <FeedHeader timelineData={feed}/>
