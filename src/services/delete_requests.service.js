861575fa8d4bbd09e4162891671398ed7e105ae9
ameyjeurkar <jeurkaramey@gmail.com>
2023-11-14 22:35:57 +0530

    feat: Delete post

--- src/services/delete_requests.service.js
+++ src/services/delete_requests.service.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const deletePost = async (imgId) => {
     // return await axios.delete(`${process.env.REACT_APP_HEROKU_URL}/post/delete_post/${imgId}`);
-    return await axios.delete(`http://localhost:8080/post/delete_post/${imgId}`);
+    return await axios.delete(`${process.env.REACT_APP_HEROKU_URL}/post/delete_post/${imgId}`);
 }
